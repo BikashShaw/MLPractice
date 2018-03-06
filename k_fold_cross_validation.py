d5fde1c32335a646b99d8499df4da0fcc8cea2e1
Bikash <bikashshaw@gmail.com>
2018-03-05 19:49:48 -0500

    k fold cross validation for parameter tuning and feature selection

--- k_fold_cross_validation.py
+++ k_fold_cross_validation.py
@@ -3,6 +3,9 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import cross_val_score
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import numpy as np
 
 # k fold cross validation for parameter tuning and model selection
 if __name__ == '__main__':
@@ -10,6 +13,7 @@ if __name__ == '__main__':
     X = iris.data
     y = iris.target
 
+    # Parameter tuning using k fold cross validation starts
     k_range = range(1, 31)
     k_scores = []
 
@@ -24,6 +28,11 @@ if __name__ == '__main__':
     plt.ylabel("Cross-Validation Accuracy")
     plt.show()
 
+    # Parameter tuning using k fold cross validation ends
+    # ----------------------------------------------------
+
+    # Model Selection using k fold cross validation starts
+
     knn = KNeighborsClassifier(n_neighbors=20)
     print("10 fold cross validation with best KNN model: ", end=" ")
     print(cross_val_score(knn, X, y, cv=10, scoring='accuracy').mean())
@@ -32,3 +41,24 @@ if __name__ == '__main__':
     print("10 fold cross validation with best Logistic Regression: ", end=" ")
     print(cross_val_score(logreg, X, y, cv=10, scoring='accuracy').mean())
 
+    # Model Selection using k fold cross validation ends
+    # ----------------------------------------------------
+
+    # Feature Selection using k fold cross validation starts
+    data = pd.read_csv("http://www-bcf.usc.edu/~gareth/ISL/Advertising.csv", index_col=0)
+    lm = LinearRegression()
+    feature_cols = ['TV', 'radio', 'newspaper']
+    X = data[feature_cols]
+    y = data['sales']
+
+    print(feature_cols, end=': ')
+    print(np.sqrt(-cross_val_score(lm, X, y, cv=10, scoring='neg_mean_squared_error')).mean())
+
+    feature_cols = ['TV', 'radio']
+    X = data[feature_cols]
+    y = data['sales']
+
+    print(feature_cols, end=': ')
+    print(np.sqrt(-cross_val_score(lm, X, y, cv=10, scoring='neg_mean_squared_error')).mean())
+
+    # Feature Selection using k fold cross validation ends
